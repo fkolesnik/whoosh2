3619f74748afdca9e8b6fdc81ecd3c56a9c43831
fkolesnik <fkolesnik@gmail.com>
2019-10-20 01:13:39 +0300

    homepage videos removed

--- src/components/ProductItemBackground.js
+++ src/components/ProductItemBackground.js
@@ -2,30 +2,15 @@ import React from "react"
 import styled from "styled-components"
 import Img from "gatsby-image"
 
-export default props => {
-  const { poster, video, hero, hasHeroVideo } = props.product
-
+export default ({product}) => {
   return (
     <Wrap>
-      {JSON.parse(hasHeroVideo) ? (
-        <Video
-          poster={poster.publicURL}
-          controls={false}
-          autoPlay
-          loop
-          muted
-          playsInline
-        >
-          <source src={video.publicURL} type="video/mp4" />
-        </Video>
-      ) : (
-        <Img
-          fluid={hero.childImageSharp.fluid}
-          imgStyle={{ objectPosition: "center top" }}
-          style={{ height: "100%", top: 0 }}
-          alt=""
-        />
-      )}
+      <Img
+        fluid={product.hero.childImageSharp.fluid}
+        imgStyle={{ objectPosition: "center top" }}
+        style={{ height: "100%", top: 0 }}
+        alt=""
+      />
     </Wrap>
   )
 }
@@ -38,19 +23,4 @@ const Wrap = styled.div`
   left: 0;
   width: 100%;
   height: 100%;
-  background-position: center;
-  background-size: cover;
-  backface-visibility: hidden;
-`
-
-const Video = styled.video`
-  position: absolute;
-  top: 0;
-  left: 50%;
-  min-width: 100%;
-  min-height: 100%;
-  width: auto;
-  height: auto;
-  transform: translateX(-50%);
-  background-size: cover;
 `
